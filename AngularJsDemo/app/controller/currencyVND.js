0d0523e47baf099eb82b8512fd101e2e7b85b19e
flyare <flyare@gmail.com>
2016-08-17 07:32:48 +0700

    try add filter currency

---
+++ AngularJsDemo/app/controller/currencyVND.js
@@ -0,0 +1,7 @@
+﻿(function(app) {
+    app.filter("currencyVND", function() {
+        return function(input) {
+            return input + " VND";
+        }
+    });
+})(angular.module("product.module"))
---
+++ AngularJsDemo/app/controller/statusFilter.js
@@ -0,0 +1,8 @@
+﻿(function(app) {
+    app.filter("statusFilter", function() {
+        return function (input) {
+            return input ? "Active" : "Inactive";
+        }
+    });
+
+})(angular.module("product.module"));
